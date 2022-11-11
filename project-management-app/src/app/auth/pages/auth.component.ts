318a610a4098cd34bcc97af9eb8c53e2a2eac049
mDjeee <isejtniyazov@mail.ru>
2022-11-11 14:41:47 +0500

    feat: add a bit style to footer

--- project-management-app/src/app/auth/pages/auth.component.ts
+++ project-management-app/src/app/auth/pages/auth.component.ts
@@ -6,16 +6,28 @@ import { FormControl, FormGroup, Validators } from "@angular/forms";
   templateUrl: './auth.component.html',
   styleUrls: ['./auth.component.scss']
 })
-export class AuthComponent {
+export class AuthComponent implements OnInit {
   hide = true;
 
   user!: FormGroup;
 
   ngOnInit(): void {
     this.user = new FormGroup({
-      name: new FormControl('', Validators.required),
-      username: new FormControl('', Validators.required),
-      password: new FormControl('', Validators.required),
+      name: new FormControl('', [Validators.required, Validators.minLength(3)]),
+      username: new FormControl('', [Validators.required, Validators.minLength(3)]),
+      password: new FormControl('', [Validators.required, Validators.minLength(8)]),
     })
+  }
+
+  get name() {
+    return this.user.get('name');
+  }
+
+  get username() {
+    return this.user.get('username');
+  }
+
+  get password() {
+    return this.user.get('password');
   }
 }

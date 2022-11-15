39ea5a14e3904ae30c35d1fc487bc9a691d43df8
Jenyajk <101403726+Jenyajk@users.noreply.github.com>
2022-11-15 23:15:42 +0500

    feat: add path error page

--- project-management-app/src/app/app.routing-module.ts
+++ project-management-app/src/app/app.routing-module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AuthComponent } from './auth/pages/auth.component';
 import { GreetingComponent } from './project-management-app/pages/greeting/greeting.component';
+import {ErrorNotFoundComponent} from "./project-management-app/pages/error-not-found/error-not-found.component";
 
 const routes: Routes = [
   { path: '', component: GreetingComponent },
-  { path: 'login', component: AuthComponent }
+  { path: 'login', component: AuthComponent },
+  { path: '**', component: ErrorNotFoundComponent}
 ]
 
 @NgModule({

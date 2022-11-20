00aefe5d6692ee64f7389f47dc0b2f4d1de06c4a
Alisher Nematov <alisher.b.nematov@gmail.com>
2022-11-20 13:06:00 +0500

    feat: add board interfaces

---
+++ project-management-app/src/app/auth/models/board.ts
@@ -0,0 +1,29 @@
+export interface IBoardBase {
+  id: string;
+  title: string;
+  description: string;
+  order: number;
+}
+
+export interface IBoard extends Omit<IBoardBase, 'order'> {
+  columns: IColumn[]
+}
+
+export interface IColumn extends Omit<IBoardBase, 'description'> {
+  tasks: ITask[];
+}
+
+export interface ITask extends Omit<IBoardBase, 'description'> {
+  userId: string;
+  files:  IFile[];
+}
+
+export interface IFile {
+  filename: string;
+  fileSize: number;
+}
+
+export interface IErrorResponse {
+  statusCode: number;
+  message: string;
+}

82f4c208c436502703eacfdfdbd17b440cad8595
Alisher Nematov <alisher.b.nematov@gmail.com>
2022-11-20 09:53:55 +0500

    feat: create main-route component

---
+++ project-management-app/src/app/project-management-app/pages/main-route/main-route.component.ts
@@ -0,0 +1,12 @@
+import { HttpClient } from "@angular/common/http";
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-main-route',
+  templateUrl: './main-route.component.html',
+  styleUrls: ['./main-route.component.scss']
+})
+export class MainRoute {
+  constructor() {
+  }
+}

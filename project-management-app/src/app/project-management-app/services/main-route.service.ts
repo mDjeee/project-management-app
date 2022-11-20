9220df10d1c5ca86d214584cf05733b10202a611
Alisher Nematov <alisher.b.nematov@gmail.com>
2022-11-20 13:06:59 +0500

    feat: add basic functions to main-route service

--- project-management-app/src/app/project-management-app/services/main-route.service.ts
+++ project-management-app/src/app/project-management-app/services/main-route.service.ts
@@ -1,7 +1,53 @@
+import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { IBoard, IBoardBase, IErrorResponse } from "src/app/auth/models/board";
+import url from "src/app/constants/baseUrl";
 
 @Injectable({
   providedIn: 'root'
 })
 export class MainRouteService {
+  public boards: Omit<IBoardBase, 'order'>[] = [];
+  public board!: IBoard;
+  constructor(private http: HttpClient) {}
+
+  public getAllBoards() {
+    this.http.get<Omit<IBoardBase, 'order'>[]>(`${url}/boards`).subscribe(
+      boards => this.boards = boards,
+      (error: IErrorResponse) => console.log(error.message)
+    )
+  }
+
+  public createBoard(title: string, description: string) {
+    const body: Omit<IBoardBase, 'id' | 'order'> = {title, description}
+    this.http.post<Omit<IBoardBase, 'order'>>(`${url}/boards`, body).subscribe(
+      board => this.boards.push(board),
+      (error: IErrorResponse) => console.log(error.message)
+    )
+  }
+
+  public getBoardById(id: string) {
+    this.http.get<IBoard>(`${url}/boards/${id}`).subscribe(
+      board => this.board = board,
+      (error: IErrorResponse) => console.log(error.message)
+    )
+  }
+
+  public deleteBoard(id: string) {
+    this.http.delete<IBoard>(`${url}/boards/${id}`).subscribe(
+      deleted => console.log(deleted),
+      (error: IErrorResponse) => console.log(error.message)
+    )
+  }
+
+  public updateBoard(id: string, title: string, description: string) {
+    const body: Omit<IBoardBase, 'id' | 'order'> = {title, description};
+    const index = this.boards.findIndex(item => item.id === id);
+    this.http.put<Omit<IBoardBase, 'order'>>(`${url}/boards/${id}`, body).subscribe(
+      board => this.boards.splice(index, 1, board),
+      (error: IErrorResponse) => console.log(error.message)
+    )
+  }
+
+
 }

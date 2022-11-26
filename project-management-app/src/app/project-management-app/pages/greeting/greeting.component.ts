a166763957294c4fd6fee1081ae381a3e1cef448
Jenyajk <101403726+Jenyajk@users.noreply.github.com>
2022-11-26 15:23:09 +0500

    fix: change size file

--- project-management-app/src/app/project-management-app/pages/greeting/greeting.component.ts
+++ project-management-app/src/app/project-management-app/pages/greeting/greeting.component.ts
@@ -3,7 +3,7 @@ import { Component } from "@angular/core";
 @Component({
   selector: 'app-greeting',
   templateUrl: './greeting.component.html',
-  styleUrls: ['./greeting.component.scss', './greeting.component-extra.scss']
+  styleUrls: ['./greeting.component.scss', './greeting.component-extra.scss', './greeting.component-extra2.scss']
 })
 export class GreetingComponent {
   AppName = 'Tagger'
